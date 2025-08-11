78b117d72a19efbe7c23cb5a7dabb892a79e8818
dafariski77 <rskdafa@gmail.com>
2025-08-11 21:57:58 +0700

    chore(theme): add initial color theme

---
+++ src/theme/colors/index.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  primary: "#ff7622",
+  lightPrimary: "#ffe1ce",
+  secondary: "",
+  white: "#ffffff",
+  green: "#059c6a",
+  input: "#f0f5fa",
+  placeholder: "#a0a5ba",
+  title: "#32343e",
+  black: "#000000",
+  subtitle: "#646982"
+} as const
